90be9d0c2f39a72a4354779ee28d4686c7743242
Sergei Galkin <qqq14582@mail.ru>
2022-03-11 14:25:30 +0300

    reload all files

---
+++ NewsPaper/newspage/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+# from newspage.models import Author
+#
+#
+# class AuthorTests(TestCase):
+#     """Author model tests."""
+#
+#     def test_str(self):
+#         author = Author(user='peter')
+#         self.assertEquals(
+#             str(author),
+#             'peter',
+#         )
